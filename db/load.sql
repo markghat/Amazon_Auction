ad3808cc838de5e12ef97067b28b14c0f3259473
Dese Elumaro <ace29@duke.edu>
2023-10-03 20:05:51 -0400

    created table for reviews and filled with dummy data

--- db/load.sql
+++ db/load.sql
@@ -19,3 +19,8 @@ SELECT pg_catalog.setval('public.purchases_id_seq',
 SELECT pg_catalog.setval('public.wishlist_id_seq',
                          (SELECT MAX(id)+1 FROM Wishlist),
                          false);
+
+\COPY Reviews FROM 'Reviews.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.reviews_id_seq',
+                         (SELECT MAX(id)+1 FROM Reviews),
+                         false);
