8ce2967ae57fdea2ad9437a1fe5a88ed6fee56ef
Samantha Ghomeshi <sbg25@duke.edu>
2023-10-05 20:48:53 -0400

    loaded data into cart table (mark helped me bc he's awesome

--- db/load.sql
+++ db/load.sql
@@ -14,3 +14,9 @@ SELECT pg_catalog.setval('public.products_id_seq',
 SELECT pg_catalog.setval('public.purchases_id_seq',
                          (SELECT MAX(id)+1 FROM Purchases),
                          false);
+
+\COPY Cart FROM 'Cart.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.cart_id_seq',
+                         (SELECT MAX(id)+1 FROM Purchases),
+                         false);
+
